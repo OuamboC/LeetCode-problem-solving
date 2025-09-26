b8ecca8adae06fefca8a3fa94a093f2a27df9b31
Canis Ouambo <118950061+OuamboC@users.noreply.github.com>
2025-09-26 19:54:04 +0100

    Time: 7 ms (1.13%), Space: 45.5 MB (28.39%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,8 +1,10 @@
 class Solution {
-    public int[] buildArray(int[] nums){
+     public int[] buildArray(int[] nums){
         ArrayList<Integer> ans = new ArrayList<>();
         for(int i = 0; i <= nums.length -1; i++){
-            ans.add(nums[nums[i]]);
+             int currentVal = nums[i];
+             int targetVal = nums[currentVal];
+             ans.add(targetVal);
 
         }
         return ans.stream().mapToInt(Integer::intValue).toArray();
